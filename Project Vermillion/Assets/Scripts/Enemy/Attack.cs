19ee19292b12a34ca9d307e6cdecfae95778a6f5
SaltyFalahi <suliflin@gmail.com>
2019-06-26 07:04:10 +0400

    Basic Attack complete - Enemy will chase and face player for a set distance, once within range the enemy will attack and damage the player. Current implementation for damage is temporary until we get the attack animation, the enemy will also only chase if the player is within his field of view. Red's Health complete - She can get damaged and has a heatlh variable.

---
+++ Project Vermillion/Assets/Scripts/Enemy/Attack.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject fist;
+
+    public int detectRange;
+    public int attackRange;
+
+    public float angleRange;
+
+    private float attackCooldown;
+    private float attackWait;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        detectRange = 10;
+        attackRange = 2;
+        angleRange = 45;
+        attackCooldown = 5;
+        attackWait = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - this.transform.position;
+        float angle = Vector3.Angle(direction, this.transform.forward);
+
+        if (Vector3.Distance(player.transform.position, this.transform.position) < detectRange && angle < angleRange)
+        {
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            if(direction.magnitude > attackRange)
+            {
+                this.transform.position += transform.forward * Time.deltaTime;
+            }
+            else
+            {
+                attackCooldown -= Time.deltaTime;
+                if(attackCooldown < 0)
+                {
+                    ActivateFist();
+                    attackWait -= Time.deltaTime;
+
+                    if(attackWait < 0)
+                    {
+                        DeactivateFist();
+                        attackCooldown = 4;
+                        attackWait = 1;
+                    }
+                }
+            }
+        }
+    }
+    public void ActivateFist()
+    {
+        fist.GetComponent<Collider>().enabled = true;
+    }
+    public void DeactivateFist()
+    {
+        fist.GetComponent<Collider>().enabled = false;
+    }
+}
---
+++ Project Vermillion/Assets/Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+        if(other.gameObject.tag == "Fist")
+        {
+            health -= 1;
+        }
+    }
+}
