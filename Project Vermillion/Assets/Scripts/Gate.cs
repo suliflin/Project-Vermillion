64ae6919f2a11c5c00fbfd52a0d9b14c23573762
Suliflin <suliflin@gmail.com>
2019-06-26 11:09:08 +0400

    Gate -  Added a function to check if there are any enemies remaining in the wave before proceeding to the next wave, added enemy spawning, added a way for the spawner to stop once all the enemies are spawned.

--- Project Vermillion/Assets/Scripts/Gate.cs
+++ Project Vermillion/Assets/Scripts/Gate.cs
@@ -20,6 +20,8 @@ public class Gate : MonoBehaviour
 
     private int nextWave = 0;
 
+    private float searchCountdown = 1f;
+
     void Start()
     {
         waveCountdown = timeBetweenWaves;     
@@ -27,9 +29,22 @@ public class Gate : MonoBehaviour
 
     void Update()
     {
+        if(state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive())
+            {
+                Debug.Log("Wave Completed");
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
         if(waveCountdown <= 0)
         {
-            if(state != SpawnState.SPAWNING)
+            if (state != SpawnState.SPAWNING)
             {
                 StartCoroutine(SpawnWave(waves[nextWave]));
             }
@@ -40,8 +55,42 @@ public class Gate : MonoBehaviour
         }
     }
 
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed");
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("All Waves Complete");
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     IEnumerator SpawnWave(Wave wave)
     {
+        Debug.Log("Spawning wave:" + wave.name);
         state = SpawnState.SPAWNING;
 
         for (int i = 0; i < wave.count; i++)
@@ -58,5 +107,6 @@ public class Gate : MonoBehaviour
     void SpawnEnemy(Transform enemy)
     {
         Debug.Log("Spawning Enemy:" + enemy.name);
+        Instantiate(enemy, transform.position, transform.rotation);
     }
 }
