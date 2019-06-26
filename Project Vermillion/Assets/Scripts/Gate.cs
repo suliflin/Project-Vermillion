b2bcbb5c0c90a22a5c27bd6e5cba19d1a1f57e2b
Suliflin <suliflin@gmail.com>
2019-06-26 10:27:55 +0400

    Gate - Created the logic for spawning enemies Wave - Created a class that allows us to have different waves Prefabs - Created a temporary prefab for Player and Enemy

---
+++ Project Vermillion/Assets/Scripts/Gate.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public enum SpawnState
+    {
+        SPAWNING,
+        WAITING,
+        COUNTING
+    }
+
+    public Wave[] waves;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    private int nextWave = 0;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;     
+    }
+
+    void Update()
+    {
+        if(waveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave(Wave wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < wave.count; i++)
+        {
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(1f/wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform enemy)
+    {
+        Debug.Log("Spawning Enemy:" + enemy.name);
+    }
+}
---
+++ Project Vermillion/Assets/Scripts/Wave.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public string name;
+
+    public Transform enemy;
+
+    public int count;
+
+    public float rate;
+
+}
