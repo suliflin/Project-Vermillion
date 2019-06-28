df7cb27fa420444f97f2c49086275f7cb7a2681e
Suliflin <suliflin@gmail.com>
2019-06-28 07:16:39 +0400

    Recall - Created one of Red's first abilities; Recall. Created a teleport script and the only input it requires is a destination.

---
+++ Project Vermillion/Assets/Scripts/Player/Recall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recall : MonoBehaviour
+{
+    public Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            transform.position = destination.position;
+        }
+    }
+}
