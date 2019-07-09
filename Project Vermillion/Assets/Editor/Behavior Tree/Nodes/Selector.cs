ccf61d5e0d592174f1bdc1dd72332a3aadc04848
Suliflin <suliflin@gmail.com>
2019-07-09 07:04:28 +0400

    Behavior Tree Basics

---
+++ Project Vermillion/Assets/Editor/Behavior Tree/Nodes/Selector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System;
+using System.IO;
+
+
+public class Selector : BaseNode
+{
+    public override void DrawWindow()
+    {
+        EditorGUILayout.LabelField("Add state to modify:");
+
+    }
+
+    public override NodeStates Evaluate(BehaviorTreeEditor bt)
+    {
+        for (int i = 0; i < treeNodes.Count; i++)
+        {
+            treeNodes[i].Evaluate(bt);
+
+            if (treeNodes[i].current == NodeStates.RUNNING)
+            {
+                current = NodeStates.RUNNING;
+                return current;
+            }
+
+            if (treeNodes[i].current == NodeStates.SUCCESS)
+            {
+                current = NodeStates.SUCCESS;
+                return current;
+            }
+        }
+        current = NodeStates.FAILURE;
+        return current;
+    }
+}
