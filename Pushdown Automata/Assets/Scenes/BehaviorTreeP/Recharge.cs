50ef1fa552116a0033a1775857b304a424403588
Kyle Laihee <k.laihee@saedubai.com>
2019-07-11 14:06:40 +0400

    no message

---
+++ Pushdown Automata/Assets/Scenes/BehaviorTreeP/Recharge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recharge : Node
+{
+    public override NodeStates NodeEvaluate(AITreeManager masterTree)
+    {
+        if (masterTree.roombaTtery < 100)
+        {
+            masterTree.roombaTtery += 0.3f;
+            currentNodeState = NodeStates.Running;
+        }
+
+        if (masterTree.roombaTtery >= 100)
+        {
+            currentNodeState = NodeStates.Success;
+        }
+
+        return currentNodeState;
+    }
+}
---
+++ Pushdown Automata/Assets/Scenes/BehaviorTreeP/Selector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : Node
+{
+    public override NodeStates NodeEvaluate(AITreeManager masterTree)
+    {
+        for (int i = 0; i < children.Count; i++)
+        {
+            NodeStates childState = children[i].NodeEvaluate(masterTree);
+
+            if(currentNodeState == NodeStates.Running && children[i].currentNodeState != NodeStates.Running)
+            {
+                continue;
+            }
+            if (childState == NodeStates.Success)
+            {
+                return NodeStates.Success;
+            }
+
+            if (childState == NodeStates.Running )
+            {
+                return NodeStates.Running;
+            }
+        }
+        return NodeStates.Failure;
+    }
+
+}
