a3cbc8dae2fab67b0132d8fb8337df12709c5ab6
Suliflin <suliflin@gmail.com>
2019-07-12 14:21:55 +0400

    Barricade - Moved barricade code into player movement (temporary)

--- Project Vermillion/Assets/Scripts/Player/Movement.cs
+++ Project Vermillion/Assets/Scripts/Player/Movement.cs
@@ -8,16 +8,16 @@ public class Movement : MonoBehaviour
     public Text appleText;
    
     public float rotatingSpeed = 130;
-    public Barricade barricade;
-  
-   //Sultan you fix this..
+    public float angleRange;
+    public float detectRange;
+
+    public bool built = false;
 
     private void Start()
     {
-        AppleCurrency.apples = 0;     
-       
+        AppleCurrency.apples = 5;
     }
-    //Update is called once per frame
+
     void Update()
     {
         //Keyboard Controls
@@ -38,10 +38,10 @@ public class Movement : MonoBehaviour
             Debug.Log("turning left");
         }
 
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            barricade.CanBuild();
-            
+            GameObject barricade = ObjectPooler.SharedInstance.GetPooledObject("Barricade");
+            CanBuild(barricade, "Barricade", 1);
         }
 
         //XINPUT
@@ -61,13 +61,60 @@ public class Movement : MonoBehaviour
             ApplesCollected();
             other.gameObject.SetActive(false);
         }
-
-        
     }
+
     //The text isn't assigned so it gives a null reference once you fix it I'll put the code back
     public void ApplesCollected()
     {
         //appleText.text = "Apples: " + AppleCurrency.apples.ToString();
     }
 
+    public bool CanBuild(GameObject build, string tag, int cost)
+    {
+        int index = 0;
+        int count = 0;
+        build.transform.position = (transform.position + (transform.forward * 2));
+
+        for (int i = 0; i < ObjectPooler.SharedInstance.pooledObjects.Count; i++)
+        {
+            if (ObjectPooler.SharedInstance.pooledObjects[i].activeInHierarchy && ObjectPooler.SharedInstance.pooledObjects[i].tag == tag)
+            {
+                index++;
+                Debug.Log(Vector3.Distance(ObjectPooler.SharedInstance.pooledObjects[i].transform.position, build.transform.position));
+                if (Vector3.Distance(ObjectPooler.SharedInstance.pooledObjects[i].transform.position, build.transform.position) > detectRange)
+                {
+                    count++;
+                    if (AppleCurrency.AppleDecrease(cost) && count == index)
+                    {
+                        build.SetActive(true);
+                        return true;
+                    }
+                    else
+                    {
+                        Debug.Log("Not enough apples");
+                        return true;
+                    }
+                }
+                else
+                {
+                    Debug.Log("Too close");
+                }
+            }
+            else if (!built)
+            {
+                if (AppleCurrency.AppleDecrease(cost))
+                {
+                    build.SetActive(true);
+                    built = true;
+                    return true;
+                }
+                else
+                {
+                    Debug.Log("Not enough apples");
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
 }
