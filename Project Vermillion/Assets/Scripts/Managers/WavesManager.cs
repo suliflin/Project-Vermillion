5dfaebaa4066bb06ebf6520d9488071475cca999
Suliflin <suliflin@gmail.com>
2019-07-13 00:39:02 +0400

    WaveManager - fixed wave spawning, added list of enemies (still need to make it spawn different types of enemies), gates spawn independently

---
+++ Project Vermillion/Assets/Scripts/Managers/WavesManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WavesManager : MonoBehaviour
+{
+    public enum SpawnState
+    {
+        SPAWNING,
+        WAITING,
+        COUNTDOWN,
+        PAUSE
+    }
+
+    public List<Gate> gates;
+    public List<GameObject> enemies;
+
+    public int numberOfWaves;
+
+    public float timeBetweenWaves;
+    public float spawnWaitTime;
+
+    public SpawnState state = SpawnState.COUNTDOWN;
+
+    private int nextWave = 0;
+
+    private float waveCountdown;
+    private float spawnCountdown;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+        spawnCountdown = spawnWaitTime;
+    }
+
+    void Update()
+    {
+        if (state == SpawnState.WAITING && spawnCountdown <= 0)
+        {
+            WaveCompleted();
+        }
+        Debug.Log(spawnCountdown);
+        if (waveCountdown <= 0)
+        {
+            if (state != SpawnState.SPAWNING && spawnCountdown == spawnWaitTime)
+            {
+                for (int i = 0; i < gates.Count; i++)
+                {
+                    StartCoroutine(gates[i].SpawnWave(gates[i].waves[nextWave]));
+                }
+                state = SpawnState.WAITING;
+            }
+            spawnCountdown -= Time.deltaTime;
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    public void WaveCompleted()
+    {
+        Debug.Log("Wave Completed");
+        state = SpawnState.COUNTDOWN;
+        waveCountdown = timeBetweenWaves;
+        spawnCountdown = spawnWaitTime;
+
+        if (nextWave + 1 > numberOfWaves - 1)
+        {
+            nextWave = 0;
+            Debug.Log("All Waves Complete");
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+}
