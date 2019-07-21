dad03d22b18177871d1c06f88b3b5bf5a0bee1b3
Kyle Laihee <k.laihee@saedubai.com>
2019-07-21 13:28:48 +0400

    Created the boss behavior tree, don't know if it works yet, gonna do that

---
+++ Project Vermillion/Assets/Scripts/Behavior Trees/BossTreeManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTreeManager : BaseBehaviorTree
+{
+    public override void Start()
+    {
+        root = new Sequence();
+
+        root.treeNodes.Add(new Succeeder());
+        root.treeNodes.Add(new Selector());
+
+        
+
+        root.treeNodes[0].treeNodes[0].treeNodes.Add(new CheckHP());
+        root.treeNodes[0].treeNodes[0].treeNodes.Add(new Retreat());
+        root.treeNodes[0].treeNodes[0].treeNodes.Add(new Heal());
+
+        root.treeNodes[1].treeNodes.Add(new Sequence());
+        root.treeNodes[1].treeNodes.Add(new Selector());
+        root.treeNodes[1].treeNodes.Add(new Sequence());
+        root.treeNodes[1].treeNodes.Add(new Selector());
+        root.treeNodes[1].treeNodes.Add(new Climb());
+
+        root.treeNodes[1].treeNodes[0].treeNodes.Add(new CheckHP());
+        root.treeNodes[1].treeNodes[0].treeNodes.Add(new Retreat());
+        root.treeNodes[1].treeNodes[0].treeNodes.Add(new Heal());
+
+        root.treeNodes[1].treeNodes[1].treeNodes.Add(new CheckCooldown());
+        root.treeNodes[1].treeNodes[1].treeNodes.Add(new CheckWolves());
+        root.treeNodes[1].treeNodes[1].treeNodes.Add(new Shield());
+
+        root.treeNodes[1].treeNodes[2].treeNodes.Add(new Sequence());
+        root.treeNodes[1].treeNodes[2].treeNodes.Add(new Sequence());
+
+        root.treeNodes[1].treeNodes[2].treeNodes[0].treeNodes.Add(new CheckPlayerSmall());
+        root.treeNodes[1].treeNodes[2].treeNodes[0].treeNodes.Add(new AttackPlayer());
+
+        root.treeNodes[1].treeNodes[2].treeNodes[1].treeNodes.Add(new CheckPlayerLarge());
+        root.treeNodes[1].treeNodes[2].treeNodes[1].treeNodes.Add(new Smash());
+
+        root.treeNodes[1].treeNodes[3].treeNodes.Add(new CheckTurret());
+        root.treeNodes[1].treeNodes[3].treeNodes.Add(new Provoke());
+
+        root.treeNodes[1].treeNodes[4].treeNodes.Add(new Sequence());
+        root.treeNodes[1].treeNodes[4].treeNodes.Add(new Return());
+
+        root.treeNodes[1].treeNodes[4].treeNodes[0].treeNodes.Add(new CheckBuild());
+        root.treeNodes[1].treeNodes[4].treeNodes[0].treeNodes.Add(new MoveToBuild());
+        root.treeNodes[1].treeNodes[4].treeNodes[0].treeNodes.Add(new AttackBuild());
+    }
+}
