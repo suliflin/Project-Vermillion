805f6beb84842893307b000a239abbeada7dab1f
Kyle Laihee <k.laihee@saedubai.com>
2019-08-14 17:45:03 +0400

    WHat a mess

---
+++ Project Vermillion/Assets/Scripts/Behavior Trees/Nodes/Check.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check : BaseNode
+{
+    public override RESULTS UpdateBehavior(BaseBehaviorTree bt)
+    {
+        return base.UpdateBehavior(bt);
+    }
+
+    public void Checker(Transform target)
+    {
+
+    }
+}
