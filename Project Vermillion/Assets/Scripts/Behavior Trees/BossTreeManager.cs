75db03875f043e9c86859275853edfae5d4f833d
Kyle Laihee <k.laihee@saedubai.com>
2019-08-14 12:03:52 +0400

    Added variables in baseBT for provoke and Shield node, both nodes are done, smash is W.I.P

---
+++ Project Vermillion/Assets/Scripts/Behavior Trees/BossTreeManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTreeManager : BaseBehaviorTree
+{
+    public override void Start()
+    {
+        sb = GetComponent<SteeringBehaviours>();
+
+        root = new Selector();
+
+        root.treeNodes.Add(new Sequence());
+        root.treeNodes.Add(new Sequence());
+        root.treeNodes.Add(new Sequence());
+
+        //root.treeNodes[0].treeNodes.Add(new CheckNode());
+        root.treeNodes[0].treeNodes.Add(new Retreat());
+
+        root.treeNodes[1].treeNodes.Add(new Shield());
+        root.treeNodes[1].treeNodes.Add(new Provoke());
+
+        root.treeNodes[2].treeNodes.Add(new Sequence());
+        root.treeNodes[2].treeNodes.Add(new Climb());
+        root.treeNodes[2].treeNodes.Add(new Smash());
+        root.treeNodes[2].treeNodes.Add(new AttackPlayer());
+
+        //root.treeNodes[2].treeNodes[0].treeNodes.Add(new CheckTree());
+        //root.treeNodes[2].treeNodes[0].treeNodes.Add(new CheckTurret());
+        //root.treeNodes[2].treeNodes[0].treeNodes.Add(new CheckPlayer());
+
+        target = spawner.nodes[0].transform;
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    public override void Update()
+    {
+        root.UpdateBehavior(this);
+    }
+}
---
+++ Project Vermillion/Assets/Scripts/Behavior Trees/Nodes/Provoke.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Provoke : BaseNode
+{
+    public override RESULTS UpdateBehavior(BaseBehaviorTree bt)
+    {
+        for (int i = 0; i < bt.detectedObjects.Count; i++)
+        {
+            if (bt.detectedObjects[i].CompareTag("Turret"))
+            {
+                bt.detectedObjects[i].GetComponent<Turret>().enemyTags = "Boss";
+                current = RESULTS.SUCCEED;
+                return current;
+            }
+            else
+            {
+                bt.detectedObjects[i].GetComponent<Turret>().enemyTags = "Enemy";
+                current = RESULTS.SUCCEED;
+                return current;
+            }
+        }
+        current = RESULTS.FAILED;
+        return current;
+    }
+}
