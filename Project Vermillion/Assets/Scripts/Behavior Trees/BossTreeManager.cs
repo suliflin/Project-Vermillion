5cd2d37f498a9f36b6965ab8254bdfb5a0640cd5
Kyle Laihee <k.laihee@saedubai.com>
2019-08-18 16:29:26 +0400

    Changed Behaviour tree, added checks and restructured for testing

--- Project Vermillion/Assets/Scripts/Behavior Trees/BossTreeManager.cs
+++ Project Vermillion/Assets/Scripts/Behavior Trees/BossTreeManager.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class BossTreeManager : BaseBehaviorTree
 {
+    public float maxRange;
+    public float enemyRange;
+
     public override void Start()
     {
         root = new Selector();
 
+        root.treeNodes.Add(new Retreat());
         root.treeNodes.Add(new Sequence());
         root.treeNodes.Add(new Sequence());
         root.treeNodes.Add(new Sequence());
 
-        //root.treeNodes[0].treeNodes.Add(new CheckToNode());
-        root.treeNodes[0].treeNodes.Add(new Retreat());
-        
+        root.treeNodes[1].treeNodes.Add(new Check("Enemy", maxRange));
         root.treeNodes[1].treeNodes.Add(new Shield());
-        root.treeNodes[1].treeNodes.Add(new Provoke());
 
-        root.treeNodes[2].treeNodes.Add(new Sequence());
-        root.treeNodes[2].treeNodes.Add(new Climb());
-        root.treeNodes[2].treeNodes.Add(new Smash());
-        //root.treeNodes[2].treeNodes.Add(new Attack());
+        root.treeNodes[2].treeNodes.Add(new Check("Enemy", maxRange));
+        root.treeNodes[2].treeNodes.Add(new Provoke());
 
-        //root.treeNodes[2].treeNodes[0].treeNodes.Add(new CheckTree());
-        //root.treeNodes[2].treeNodes[0].treeNodes.Add(new CheckTurret());
-        //root.treeNodes[2].treeNodes[0].treeNodes.Add(new CheckPlayer());
+        root.treeNodes[3].treeNodes.Add(new Selector());
+        root.treeNodes[3].treeNodes.Add(new Climb());
+        root.treeNodes[3].treeNodes.Add(new Selector());
+
+        root.treeNodes[3].treeNodes[0].treeNodes.Add(new Check("Tree", maxRange));
+        root.treeNodes[3].treeNodes[0].treeNodes.Add(new Check("Turret", maxRange));
+        root.treeNodes[3].treeNodes[0].treeNodes.Add(new Check("Player", maxRange));
+
+        root.treeNodes[3].treeNodes[2].treeNodes.Add(new Smash());
+        root.treeNodes[3].treeNodes[2].treeNodes.Add(new Attack());
     }
 }
