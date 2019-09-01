0f1afa6987edb2be415ed3af6214d37ac9d605a3
OderaIgwe <o.igwe@saedubai.com>
2019-09-01 22:55:14 +0400

    Particle Effects and Textures Put again.

--- Project Vermillion/Assets/Script/Player/CrossbowController.cs
+++ Project Vermillion/Assets/Script/Player/CrossbowController.cs
@@ -8,14 +8,21 @@ public class CrossbowController : MonoBehaviour
     public Transform firePointB;
 
     public bool isFiring;
+ 
 
     public float boltSpeed;
     public float timeBetweenShots;
     public float lifeTime;
-
+    public List<GameObject> vfx = new List<GameObject>();
+    GameObject particleToSpawn;
     private int crossbow;
 
     private float shotCounter;
+
+    private void Start()
+    {
+        particleToSpawn = vfx[0]; 
+    }
 
     void Update()
     {
@@ -29,11 +36,32 @@ public class CrossbowController : MonoBehaviour
                 GameObject newBolt = ObjectPooler.SharedInstance.SpawnFromPool("Bolt", AlternateShot().position, AlternateShot().rotation);
                 newBolt.GetComponent<BoltController>().speed = boltSpeed;
                 newBolt.GetComponent<BoltController>().duration = lifeTime;
+                SpawnParticle();
             }
         }
         else
         {
             shotCounter = 0;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+        }
+    }
+
+    void SpawnParticle()
+    {
+        GameObject vfx;
+        GameObject vfxL;
+
+        if(firePointA != null)
+        {
+            vfx = Instantiate(particleToSpawn, firePointA.transform.position, firePointA.transform.rotation);
+        }
+        if (firePointB != null)
+        {
+            vfxL = Instantiate(particleToSpawn, firePointB.transform.position, firePointB.transform.rotation);
         }
     }
 
